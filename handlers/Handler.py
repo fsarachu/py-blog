4d17871b154bd4e49b40cf78432a45edbed3f6e1
Franco Sarachu <fsarachu@gmail.com>
2016-11-14 17:09:49 -0300

    Add Handler class

---
+++ handlers/Handler.py
@@ -0,0 +1,16 @@
+import jinja2
+import os
+
+import webapp2
+
+
+class Handler(webapp2.RequestHandler):
+    TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../templates')
+    JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR), autoescape=True)
+
+    def write(self, *args, **kwargs):
+        self.response.write(*args, **kwargs)
+
+    def render(self, template_name, **kwargs):
+        template = self.JINJA_ENV.get_template(template_name)
+        self.write(template.render(kwargs))
