2714853a24bff33dd034a73db8737fb769b9f0de
Franco Sarachu <fsarachu@gmail.com>
2016-11-17 14:20:00 -0300

    Designed signup page

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -3,11 +3,10 @@ from Handler import Handler
 
 class MainHandler(Handler):
     def get(self):
-        self.render('index.html')
-        # user_id = self.request.cookies.get('user_id')
-        #
-        # if user_id:
-        #     # Get user from db and then..
-        #     self.render('index.html')
-        # else:
-        #     self.redirect('/signup')
+        user_id = self.request.cookies.get('user_id')
+
+        if user_id:
+            # Get user from db and then..
+            self.render('index.html')
+        else:
+            self.redirect('/signup')
