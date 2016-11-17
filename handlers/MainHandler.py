bbdc54d699ec199439d2349437e2bcffb7702316
Franco Sarachu <fsarachu@gmail.com>
2016-11-17 13:34:03 -0300

    Redirect if no cookie is found

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -3,4 +3,10 @@ from Handler import Handler
 
 class MainHandler(Handler):
     def get(self):
-        self.render('index.html')
+        user_id = self.request.cookies.get('user_id')
+
+        if user_id:
+            # Get user from db and then..
+            self.render('index.html')
+        else:
+            self.redirect('/signup')
