1d25a2f559332fe9e6ed9b7b3e300e1b753fb9a6
Franco Sarachu <fsarachu@gmail.com>
2016-11-16 22:21:25 -0300

    Rename class variable to satisfy python naming convention

--- handlers/SignupHandler.py
+++ handlers/SignupHandler.py
@@ -3,7 +3,7 @@ from validators import SignupValidator
 
 
 class SignupHandler(Handler):
-    validator = SignupValidator.SignupValidator()
+    VALIDATOR = SignupValidator.SignupValidator()
 
     def get(self):
         self.render('signup.html')
@@ -17,18 +17,18 @@ class SignupHandler(Handler):
 
         params = dict(username=username, email=email)
 
-        if not self.validator.valid_username(username):
+        if not self.VALIDATOR.valid_username(username):
             params['error_username'] = 'Username is not valid!'
             have_error = True
 
-        if not self.validator.valid_password(password):
+        if not self.VALIDATOR.valid_password(password):
             params['error_password'] = 'Password is not valid!'
             have_error = True
         elif password != verify:
             params['error_verify'] = 'Passwords do not match!'
             have_error = True
 
-        if not self.validator.valid_email(email):
+        if not self.VALIDATOR.valid_email(email):
             params['error_email'] = 'Email is not valid!'
             have_error = True
 
