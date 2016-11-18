9bc388e9d468a7c03965b4fc9ffad63932238b79
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 12:49:14 -0300

    Fix unicode string error

--- helpers/CookieHelper.py
+++ helpers/CookieHelper.py
@@ -1,6 +1,5 @@
 import hmac
 import random
-import string
 
 
 class CookieHelper:
@@ -9,7 +8,7 @@ class CookieHelper:
 
     @staticmethod
     def hash_str(secret_key, message):
-        return hmac.new(secret_key, message).hexdigest()
+        return hmac.new(str(secret_key), str(message)).hexdigest()
 
     @staticmethod
     def make_secure_value(s, salt=None):
@@ -18,11 +17,11 @@ class CookieHelper:
         return '{}-{}{}'.format(s, salt, CookieHelper.hash_str(salt, s))
 
     @staticmethod
-    def check_secure_value(s):
-        value = s.split('-')[0]
-        salt = (s.split('-')[1])[:5]
-        return value if s == CookieHelper.make_secure_value(value, salt) else None
+    def check_secure_value(cookie_str):
+        value = cookie_str.split('-')[0]
+        salt = (cookie_str.split('-')[1])[:5]
+        return value if cookie_str == CookieHelper.make_secure_value(value, salt) else None
 
     @staticmethod
     def make_salt(length=5):
-        return ''.join(random.choice(string.hexdigits) for _ in range(length))
+        return ''.join(random.choice('1234567890abcdef') for _ in range(length))
