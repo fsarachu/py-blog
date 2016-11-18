eaa4d04a8f5777dfd1f1146d0164a3fa7eb84906
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 16:56:17 -0300

    Check if user already exists

--- handlers/SignupHandler.py
+++ handlers/SignupHandler.py
@@ -1,4 +1,5 @@
 from Handler import Handler
+from entities import User
 from helpers import CookieHelper
 from validators import *
 
@@ -10,14 +11,23 @@ class SignupHandler(Handler):
     def post(self):
         have_error = False
         username = self.request.get('username')
+        email = self.request.get('email')
         password = self.request.get('password')
         verify = self.request.get('verify')
-        email = self.request.get('email')
 
         params = dict(username=username, email=email)
 
         if not SignupValidator.valid_username(username):
             params['error_username'] = 'Username is not valid!'
+            have_error = True
+        else:
+            existing_user = User.all().filter('username =', username).get()
+            if existing_user:
+                params['error_username'] = 'Username already exists!'
+                have_error = True
+
+        if not SignupValidator.valid_email(email):
+            params['error_email'] = 'Email is not valid!'
             have_error = True
 
         if not SignupValidator.valid_password(password):
@@ -25,10 +35,6 @@ class SignupHandler(Handler):
             have_error = True
         elif password != verify:
             params['error_verify'] = 'Passwords do not match!'
-            have_error = True
-
-        if not SignupValidator.valid_email(email):
-            params['error_email'] = 'Email is not valid!'
             have_error = True
 
         if have_error:
