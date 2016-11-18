8e1f20943208491269c5472c6b25674b32f46988
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 01:13:25 -0300

    Add CookieHelper class

---
+++ helpers/CookieHelper.py
@@ -0,0 +1,32 @@
+import hmac
+import random
+import string
+
+
+class CookieHelper:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def hash_str(secret_key, message):
+        return hmac.new(secret_key, message).hexdigest()
+
+    @staticmethod
+    def make_secure_value(s, salt=None):
+        if not salt:
+            salt = CookieHelper.make_salt()
+        return '{}-{},{}'.format(s, salt, CookieHelper.hash_str(salt, s))
+
+    @staticmethod
+    def check_secure_value(s):
+        value = s.split('-')[0]
+        salt = (s.split('-')[1]).split(',')[0]
+
+        if s == CookieHelper.make_secure_value(value, salt):
+            return value
+        else:
+            return None
+
+    @staticmethod
+    def make_salt(length=5):
+        return ''.join(random.choice(string.ascii_letters) for _ in range(length))
---
+++ helpers/__init__.py
@@ -0,0 +1,1 @@
+from CookieHelper import CookieHelper
