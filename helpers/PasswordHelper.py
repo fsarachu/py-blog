d4de2c8278f98724db20dbb5c04547839cb70b6b
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 17:16:37 -0300

    Add PasswordHelper class

---
+++ helpers/PasswordHelper.py
@@ -0,0 +1,6 @@
+class PasswordHelper:
+    def make_password_hash(self, plaintext_password):
+        pass
+
+    def check_password_hash(self, hash):
+        pass
--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,2 +1,3 @@
 from CookieHelper import CookieHelper
 from HashHelper import HashHelper
+from PasswordHelper import PasswordHelper
