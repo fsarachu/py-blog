6f5266c7b71021d601981916740041289d0516c1
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 23:51:40 -0300

    Define by_id classmethod

--- entities/User.py
+++ entities/User.py
@@ -5,3 +5,7 @@ class User(db.Model):
     username = db.StringProperty(required=True)
     email = db.EmailProperty()
     password = db.StringProperty(required=True)
+
+    @classmethod
+    def by_id(cls, user_id):
+        return User.get_by_id(user_id)
