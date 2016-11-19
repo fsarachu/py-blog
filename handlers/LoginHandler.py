62d4985583fc67ed104cccf4953db550933b7b40
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 23:24:35 -0300

    define do_login method

--- handlers/LoginHandler.py
+++ handlers/LoginHandler.py
@@ -7,6 +7,4 @@ class LoginHandler(Handler):
         self.render('login.html')
 
     def do_login(self, user):
-        self.response.headers.add_header('Set-Cookie',
-                                         'username={}; Path=/'.format(
-                                             CookieHelper.make_secure_value(user.key().id())))
+        CookieHelper.set_secure_cookie(self, 'user_id', user.key().id())
