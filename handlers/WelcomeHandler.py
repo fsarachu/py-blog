a6968eaefabdf81a8a3f99788345c8d4a91255d7
Franco Sarachu <fsarachu@gmail.com>
2016-11-19 00:06:25 -0300

    Refactor WelcomeHandler get method

--- handlers/WelcomeHandler.py
+++ handlers/WelcomeHandler.py
@@ -1,15 +1,9 @@
 from Handler import Handler
-from helpers import CookieHelper
 
 
 class WelcomeHandler(Handler):
     def get(self):
-        username_cookie_str = self.request.cookies.get('username')
-        if not username_cookie_str:
-            self.redirect('/signup')
+        if self.user:
+            self.render('index.html', username=self.user.username)
         else:
-            username = CookieHelper.check_secure_value(username_cookie_str)
-            if not username:
-                self.redirect('/signup')
-            else:
-                self.render('index.html', username=username)
+            self.redirect('/signup')
