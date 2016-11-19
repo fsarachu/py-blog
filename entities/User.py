3bfdbb66ca9878119313cd10f8d41e5debdb71b4
Franco Sarachu <fsarachu@gmail.com>
2016-11-18 23:50:11 -0300

    Make email property not required

--- entities/User.py
+++ entities/User.py
@@ -3,5 +3,5 @@ from google.appengine.ext import db
 
 class User(db.Model):
     username = db.StringProperty(required=True)
-    email = db.EmailProperty(required=True)
+    email = db.EmailProperty()
     password = db.StringProperty(required=True)
