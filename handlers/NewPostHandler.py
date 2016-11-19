03287f6660f90fb957976481e3f1ff0958652318
Franco Sarachu <fsarachu@gmail.com>
2016-11-19 16:59:53 -0300

    Add basic NewPostHandler class

---
+++ handlers/NewPostHandler.py
@@ -0,0 +1,19 @@
+from Handler import Handler
+from entities import Post
+
+
+class NewPostHandler(Handler):
+    def get(self):
+        self.render('new_post.html')
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            new_post = Post(subject=subject, content=content)
+            new_post.put()
+            self.redirect('/' + str(new_post.key().id()))
+        else:
+            error = 'You must provide both subject and content.'
+            self.render('new_post.html', subject=subject, content=content, error=error)
