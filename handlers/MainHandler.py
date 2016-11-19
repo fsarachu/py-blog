09eebbbbf017352fb6a37cb56a8eeac015b7614b
Franco Sarachu <fsarachu@gmail.com>
2016-11-19 16:54:19 -0300

    Load posts in main page

--- handlers/MainHandler.py
+++ handlers/MainHandler.py
@@ -1,9 +1,11 @@
 from Handler import Handler
+from entities import Post
 
 
 class MainHandler(Handler):
     def get(self):
         if self.user:
-            self.render('index.html', user=self.user)
+            posts = Post.all().order('-created').fetch(limit=10)
+            self.render('index.html', user=self.user, posts=posts)
         else:
             self.redirect('/signup')
