00df61903b76e30471e5329fecd7162904611eee
Franco Sarachu <fsarachu@gmail.com>
2016-11-19 17:00:43 -0300

    Add /newpost route

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 from LoginHandler import LoginHandler
 from LogoutHandler import LogoutHandler
 from MainHandler import MainHandler
+from NewPostHandler import NewPostHandler
 from SignupHandler import SignupHandler
--- main.py
+++ main.py
@@ -6,5 +6,6 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/signup', SignupHandler),
     ('/login', LoginHandler),
-    ('/logout', LogoutHandler)
+    ('/logout', LogoutHandler),
+    ('/newpost', NewPostHandler)
 ], debug=True)
