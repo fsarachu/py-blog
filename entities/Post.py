ed122b52bdc8b5b7339d4ae135d857d67accfbed
Franco Sarachu <fsarachu@gmail.com>
2016-11-19 21:37:02 -0300

    Add user-post relationship

--- entities/Post.py
+++ entities/Post.py
@@ -1,7 +1,10 @@
 from google.appengine.ext import db
 
+from entities import User
+
 
 class Post(db.Model):
+    author = db.ReferenceProperty(User, collection_name='posts')
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
