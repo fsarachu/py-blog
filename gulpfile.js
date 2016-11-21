a3192fba4b87841ac924447421d19ff1a5d17e52
Franco Sarachu <fsarachu@gmail.com>
2016-11-21 15:09:21 -0300

    Define bower-css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
+/* Bower components are hardcoded just beacause this is a small thing */
 
+gulp.task('bower-css', function () {
+    return gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('./public/css/vendor.css'))
 });
