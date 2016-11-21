022a0d41bc01924ba3d6c4f34c78664b14133590
Franco Sarachu <fsarachu@gmail.com>
2016-11-21 16:39:47 -0300

    Config dotdotdot jQuery plugin

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
+    /* Dotdotdot plugin configuration*/
 
+    $(".post-preview").dotdotdot({
+        ellipsis: '... ',
+        wrap: 'word',
+        fallbackToLetter: true,
+        after: "a.readmore",
+        watch: true,
+        height: null,
+        tolerance: 0,
+        lastCharacter: {
+            remove: [' ', ',', ';', '.', '!', '?'],
+            noEllipsis: []
+        }
+    });
 });
