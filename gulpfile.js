5d76e9a043d623fdbcf4ec7c17eaa98c5ed3e594
Franco Sarachu <fsarachu@gmail.com>
2016-11-21 15:21:27 -0300

    Fix bower-css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 
 /* Bower components are hardcoded just beacause this is a small thing */
 
+
 gulp.task('bower-css', function () {
     return gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
-        .pipe(gulp.dest('./public/css/vendor.css'))
+        .pipe(rename('vendor.css'))
+        .pipe(gulp.dest('./public/css/'))
 });
